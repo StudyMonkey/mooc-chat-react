58e1e55caa275da0b05fd75ab998444350246a79
未来_加油 <741804631@qq.com>
2019-06-12 21:21:41 +0800

    项目初始化，并添加了babel-plugin-import和antd-design依赖

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'antd'
+
+// import logo from './logo.svg';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <div>
+        This is react
+        <Button type="primary">测试</Button>
+      </div>
+    )
+  }
+}
+
